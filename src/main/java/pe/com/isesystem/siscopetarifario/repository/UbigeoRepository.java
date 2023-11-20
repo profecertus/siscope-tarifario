5b8c0b7eb80e1a326c979708f73d5833b2c0343b
edwinBarrientosRetuerto <edwin.barrientos.retuerto@gmail.com>
2023-11-20 14:18:17 -0500

    Adecuación de Grabado de Tarifas.

--- src/main/java/pe/com/isesystem/siscopetarifario/repository/UbigeoRepository.java
+++ src/main/java/pe/com/isesystem/siscopetarifario/repository/UbigeoRepository.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Repository
 public interface UbigeoRepository extends JpaRepository<Ubigeo, String> {
 
-    @Query(value = "select u.* from ubigeo u inner join planta p on u.cod_ubigeo = p.cod_ubigeo where p.estado_reg = true and u.estado_reg = true",nativeQuery = true)
+    @Query(value = "select DISTINCT u.* from ubigeo u inner join planta p on u.cod_ubigeo = p.cod_ubigeo where p.estado_reg = true and u.estado_reg = true",nativeQuery = true)
     List<Ubigeo> getAllUbigeo();
 
 }
